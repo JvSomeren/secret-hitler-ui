b4983db88690759623f1c203aca48c05d9d0e685
Joost van Someren <joost.v.someren@gmail.com>
2019-03-29 23:52:01 +0100

    add barebones `back`-component
    Needs styling and potentially additional logic

---
+++ src/components/_sh-back.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sh-back" @click="_click">
+    <slot v-if="!!$slots.default" />
+    <span v-else>{{ text }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component
+export default class Back extends Vue {
+  @Prop({type: String, default: '< BACK'})
+  private text: string;
+
+  private _click() {
+    this.$router.back();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .sh-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    padding: 6px 0 0 6px;
+  }
+</style>
