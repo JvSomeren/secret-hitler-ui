c286f99a3c3bffb9dbed4cd7d82dec9835f488dd
Joost van Someren <joost.v.someren@gmail.com>
2019-03-29 23:26:42 +0100

    add `link` and `button` component

---
+++ src/components/_sh-link.vue
@@ -0,0 +1,23 @@
+<template>
+  <ShButton :click="_click">
+    <slot />
+  </ShButton>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component
+export default class Link extends Vue {
+  @Prop([String, Location])
+  private to!: string | object;
+
+  private _click() {
+    this.$router.push(this.to);
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
