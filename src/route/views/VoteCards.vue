9e43d95e53b017f8be630d7180750fa7686cee65
Joost van Someren <joost.v.someren@gmail.com>
2019-03-30 22:33:52 +0100

    add `VoteCards`-view

---
+++ src/route/views/VoteCards.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="sh-vote-cards">
+    <ShBack />
+
+    <VoteCard ja />
+    <VoteCard nein />
+  </section>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import VoteCard from '@/components/VoteCard.vue';
+
+@Component({
+  components: {
+    VoteCard,
+  },
+})
+export default class VoteCards extends Vue {}
+</script>
+
+<style scoped lang="scss">
+  .sh-vote-cards {
+    width: 100%;
+    padding-top: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: stretch;
+    overflow: hidden;
+  }
+</style>
