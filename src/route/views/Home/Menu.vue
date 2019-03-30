62f74cfb1ff04fbaac11fcf24e0c638ce6e7813e
Joost van Someren <joost.v.someren@gmail.com>
2019-03-30 22:35:49 +0100

    add `Menu`-view

---
+++ src/route/views/Home/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <ShLink to="standalone">Standalone</ShLink>
+
+    <h2>ONLINE</h2>
+    <ShLink to="/" disabled>Host game</ShLink>
+    <ShLink to="/" disabled>Join game</ShLink>
+
+    <ShLink to="rules" class="sh-bottom">Rules</ShLink>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+})
+export default class Menu extends Vue {}
+</script>
+
+<style scoped lang="scss">
+</style>
