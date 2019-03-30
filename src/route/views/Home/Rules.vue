e0e5f53ae6aa3dc0e2b9b728dc1602e4da28664e
Joost van Someren <joost.v.someren@gmail.com>
2019-03-30 22:34:33 +0100

    add `Rules`-view

---
+++ src/route/views/Home/Rules.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <ShBack />
+
+    <div class="sh-rules">
+      <!--<h3>ELECTION</h3>-->
+      <!--<ol>-->
+        <!--<li>Pass the Presidential Candidacy</li>-->
+        <!--<li>Nominate a Chancellor</li>-->
+      <!--</ol>-->
+    </div>
+
+    <ShLink
+      class="sh-bottom"
+      :file="`${publicPath}Secret_Hitler_Rules.pdf`">Full version [PDF]</ShLink>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Rules extends Vue {
+  private publicPath: string = process.env.BASE_URL;
+}
+</script>
+
+<style scoped lang="scss">
+  .sh-rules {
+    flex: 1;
+    align-self: stretch;
+    height: 100%;
+    overflow-y: scroll;
+
+    font-family: 'Courier', Courier, monospace;
+  }
+</style>
