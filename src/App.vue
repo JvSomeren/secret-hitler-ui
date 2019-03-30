758ab54cd8f35eb87ec325995f78de42d0b87f3e
Joost van Someren <joost.v.someren@gmail.com>
2019-03-30 22:31:16 +0100

    overhaul home

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <main id="app">
+    <router-view />
+  </main>
 </template>
 
 <style lang="scss">
 @import 'normalize-scss';
 @include normalize();
+
+* {
+  user-select: none;
+}
 
 html,
 body {
@@ -21,17 +21,46 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: #FBB969;
+
+  height: 100%;
+  display: flex;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.root-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  flex: 1;
+}
+
+.header-logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.main {
+  flex: 3;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+h2 {
+  font-family: 'Futura', sans-serif;;
+  font-weight: 700;
+  color: #434343;
+  margin-top: 0;
+  margin-bottom: 8px;
+}
+
+.sh-bottom {
+  margin-top: auto;
 }
 </style>
---
+++ src/route/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root-container">
+    <section class="header header-logo">
+      <LogoVertical class="logo" />
+    </section>
+
+    <router-view class="main"></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import LogoVertical from '@/assets/logo-vertical.svg';
+
+@Component({
+  components: {
+    LogoVertical,
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.logo {
+  height: 100px;
+
+  #letters {
+    fill: #F2654B;
+  }
+}
+</style>
