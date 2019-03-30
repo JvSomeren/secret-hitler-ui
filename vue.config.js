57b2bbcf7572c3d1f5df3fed6092175e8296f8b5
Joost van Someren <joost.v.someren@gmail.com>
2019-03-30 22:21:43 +0100

    update config files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+const manifestJSON = require('./public/manifest');
+
 module.exports = {
+  pwa: {
+    themeColor: manifestJSON.theme_color,
+    msTileColor: manifestJSON.background_color
+  },
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
 
