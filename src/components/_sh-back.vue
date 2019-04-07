3472e887f9074e1f8e4f3082013a8fa8421783ba
Joost van Someren <joost.v.someren@gmail.com>
2019-04-07 21:57:40 +0200

    adapt `ShBack`-component for navbar

--- src/components/_sh-back.vue
+++ src/components/_sh-back.vue
@@ -1,11 +1,20 @@
 <template>
   <div v-if="!displayPrompt" class="sh-back" @click="_click">
     <slot v-if="!!$slots.default" />
-    <span v-else>{{ text }}</span>
+    <span v-else-if="text !== ''">{{ _text }}</span>
+    <template v-else v-html="_text">
+      <Clear v-if="endPrompt || endGame" />
+      <ArrowBack v-else />
+      <span>{{ _text }}</span>
+    </template>
   </div>
 
-  <section v-else class="sh-back sh-prompt__wrapper">
-    <div class="sh-back" @click="cancel"><span>Cancel</span></div>
+  <section v-else class="sh-prompt__wrapper">
+    <div class="sh-back sh-back-cancel" @click="cancel">
+      <ArrowBack />
+      <span>Cancel</span>
+    </div>
+
     <div class="sh-prompt">
       <h2>Do you want to close or end the game?</h2>
 
@@ -22,10 +31,17 @@
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-property-decorator';
 import {Mutation} from 'vuex-class';
+import ArrowBack from '@/assets/arrow-back.svg';
+import Clear from '@/assets/clear.svg';
 
-@Component
+@Component({
+  components: {
+    ArrowBack,
+    Clear,
+  },
+})
 export default class Back extends Vue {
-  @Prop({type: String, default: '< BACK'})
+  @Prop({type: String, default: ''})
   private text?: string;
 
   @Prop({type: Boolean, default: false})
@@ -42,6 +58,15 @@ export default class Back extends Vue {
 
   private displayPrompt = false;
   private endGameButtonDisabled = false;
+
+  get _text() {
+    if (this.text !== '') return this.text;
+    if (this.gotoHome) return 'HOME';
+    if (this.endPrompt
+      || this.endGame) return 'EXIT';
+
+    return 'BACK';
+  }
 
   private _endGame() {
     if (this.endGameButtonDisabled) return;
@@ -81,39 +106,60 @@ export default class Back extends Vue {
   $bg-color: #FBB969;
 
   .sh-back {
+    display: flex;
+    align-items: center;
+    font-family: 'Courier', Courier, monospace;
+
+    svg {
+      width: unset;
+      height: 17px;
+      padding-left: 3px;
+    }
+
+    span {
+      font-size: 18px;
+    }
+  }
+
+  .sh-prompt__wrapper {
     position: absolute;
     z-index: 999;
     top: 0;
     left: 0;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
     background-color: $bg-color;
+    width: calc(100% - 12px);
+    height: 100%;
+    padding: 0 6px 0 6px;
 
-    padding: 6px 0 0 6px;
+    .sh-back-cancel {
+      position: absolute;
+      top: 0;
+      left: 0;
 
-    &.sh-prompt__wrapper {
+      height: 40px;
       display: flex;
-      flex-direction: column;
-      justify-content: center;
+      align-items: center;
+    }
+  }
 
-      background-color: $bg-color;
-      width: calc(100% - 12px);
-      height: 100%;
-      padding: 0 6px 0 6px;
+  .sh-prompt {
+    display: flex;
+    flex-direction: column;
+
+    height: 30%;
+
+    h2 {
+      text-align: center;
     }
 
-    .sh-prompt {
+    .sh-prompt-buttons {
       display: flex;
-      flex-direction: column;
-
-      height: 30%;
-      
-      h2 {
-        text-align: center;
-      }
-
-      .sh-prompt-buttons {
-        display: flex;
-        justify-content: space-around;
-      }
+      justify-content: space-around;
     }
   }
 </style>
