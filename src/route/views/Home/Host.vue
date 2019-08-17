49f23f5c4b03e6663d5cf4c5e7ef687ea18a3c19
Joost van Someren <joost.v.someren@gmail.com>
2019-08-17 20:49:14 +0200

    update Host and Join view

--- src/route/views/Home/Host.vue
+++ src/route/views/Home/Host.vue
@@ -1,12 +1,53 @@
 <template>
-  <p>Host</p>
+  <section>
+    <h2>PICK YOUR USERNAME</h2>
+
+    <ShInput
+      v-model="name"
+      :value="name"
+      placeholder="Player" />
+
+    <ShButton
+      class="sh-bottom"
+      :disabled="disabled"
+      @click.native="_create">Create</ShButton>
+  </section>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import {namespace} from 'vuex-class';
+import {mapFields} from 'vuex-map-fields';
+import {onlineMutations} from '@/state/modules/Online/mutations';
 
-@Component({})
+const online = namespace('online');
+
+@Component({
+  computed: {
+    ...mapFields(`online`, ['user.name']),
+  },
+})
 export default class Host extends Vue {
+  @online.State((state) => state.user.name)
+  private username!: string;
+
+  @online.Action('createLobby')
+  private createLobby!: () => void;
+
+  @online.Mutation(onlineMutations.resetState)
+  private resetState!: () => void;
+
+  private disabled = false;
+
+  private _create(e: MouseEvent) {
+    if (this.username === '') return;
+
+    this.createLobby();
+  }
+
+  private created() {
+    this.resetState();
+  }
 }
 </script>
 
--- src/route/views/Home/Join.vue
+++ src/route/views/Home/Join.vue
@@ -1,14 +1,74 @@
 <template>
-  <p>join</p>
+  <section>
+    <h2>username + lobby ID</h2>
+
+    <ShInput
+      v-model="name"
+      :value="name"
+      placeholder="Player" />
+
+    <ShInput
+      class="lobby-input"
+      v-model="lobbyId"
+      :value="lobbyId"
+      placeholder="Lobby ID" />
+
+    <ShButton
+      class="sh-bottom"
+      :disabled="disabled"
+      @click.native="_connect">Connect</ShButton>
+  </section>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import {namespace} from 'vuex-class';
+import {mapFields} from 'vuex-map-fields';
 
-@Component({})
+const online = namespace('online');
+
+@Component({
+  computed: {
+    ...mapFields(`online`, ['user.name']),
+  },
+})
 export default class Join extends Vue {
+  @online.State((state) => state.user.name)
+  private username!: string;
+
+  @online.Action('connectToLobby')
+  private connectToLobby!: (lobbyID: string) => Promise<boolean>;
+
+  private disabled = false;
+  private lobbyId: string = '';
+
+  private validateLobbyId() {
+    if (this.lobbyId.length !== 4) return false;
+    if (/[^a-z0-9]/gi.test(this.lobbyId)) return false;
+
+    return true;
+  }
+
+  private _connect(e: MouseEvent) {
+    if (this.username === '') return;
+    if (!this.validateLobbyId()) return;
+    // check if lobbyid is defined
+
+    this.lobbyId = this.lobbyId.toUpperCase();
+
+    this.disabled = true;
+    this.connectToLobby(this.lobbyId)
+      .then((success: boolean) => this.disabled = success);
+  }
 }
 </script>
 
 <style scoped lang="scss">
+  .lobby-input {
+    text-transform: uppercase;
+
+    &::placeholder {
+      text-transform: capitalize;
+    }
+  }
 </style>
