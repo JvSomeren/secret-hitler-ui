900d65f3a9fdf2fdf7f38c22315b8a6c5b8986ab
Joost van Someren <joost.v.someren@gmail.com>
2019-08-20 09:32:35 +0200

    set host peerId of lobby on page refresh

--- src/route/views/Online.vue
+++ src/route/views/Online.vue
@@ -4,9 +4,24 @@
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import {namespace} from 'vuex-class';
+
+const online = namespace('online');
 
 @Component({})
 export default class Online extends Vue {
+  @online.State((state) => state.user.isHost)
+  private isHost!: boolean;
+
+  @online.State((state) => state.user.peerId)
+  private peerId!: string | null;
+
+  @online.Action('registerPeerAsHost')
+  private registerPeerAsHost!: () => void;
+
+  private created() {
+    if (this.isHost && this.$peer.id !== this.peerId) this.registerPeerAsHost();
+  }
 }
 </script>
 
