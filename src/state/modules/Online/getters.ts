50ae468130414066c2acd383f1667f724da7459e
Joost van Someren <joost.v.someren@gmail.com>
2019-08-20 09:33:37 +0200

    update online getters and mutations

--- src/state/modules/Online/getters.ts
+++ src/state/modules/Online/getters.ts
@@ -1,8 +1,68 @@
 import {GetterTree} from 'vuex';
-import {OnlineState} from '@/state/modules/Online/types';
+import {OnlinePlayer, OnlineState} from '@/state/modules/Online/types';
 import {RootState} from '@/state/types';
 import {getField} from 'vuex-map-fields';
+import {GameStatus} from '@/state/modules/Standalone/types';
 
 export const getters: GetterTree<OnlineState, RootState> = {
   getField,
+
+  user(state): object {
+    const { user } = state;
+
+    return user;
+  },
+
+  gameState(state): object {
+    const { lobbyId, playerCount, status, view } = state;
+
+    return {
+      lobbyId,
+      playerCount,
+      status,
+      view,
+    };
+  },
+
+  gameStatus(state): string {
+    const { status } = state;
+
+    switch (status) {
+      case GameStatus.CHOOSING_INITIAL_PRESIDENT:
+        return 'Initial president';
+      case GameStatus.NOMINATING_CHANCELLOR:
+        return 'Nominating chancellor';
+      case GameStatus.VOTING_ON_GOVERNMENT:
+        return 'Election';
+      case GameStatus.PRESIDENT_DISCARDING_POLICY:
+        return 'President discards';
+      case GameStatus.CHANCELLOR_DISCARDING_POLICY:
+        return 'Chancellor discards';
+      case GameStatus.INVESTIGATING_LOYALTY:
+        return 'Investigate Loyalty';
+      case GameStatus.CALLING_SPECIAL_ELECTION:
+        return 'Call Special Election';
+      case GameStatus.POLICY_PEEKING:
+        return 'Policy Peek';
+      case GameStatus.EXECUTING:
+        return 'Execution';
+      case GameStatus.CALLING_VETO:
+        return 'Veto';
+      case GameStatus.STOPPED:
+      case GameStatus.INITIALIZING:
+      case GameStatus.SHOWING_ROLES:
+      case GameStatus.PRE_GAME:
+      case GameStatus.EXECUTIVE_ACTION:
+      case GameStatus.ENACT_TOP_POLICY:
+      case GameStatus.GAME_END:
+      default:
+        return '';
+    }
+  },
+
+  allPlayers(state): OnlinePlayer[] {
+    const { players } = state;
+
+    return players;
+  },
 };
--- src/state/modules/Online/mutations.ts
+++ src/state/modules/Online/mutations.ts
@@ -1,7 +1,97 @@
 import {MutationTree} from 'vuex';
-import {OnlineState} from '@/state/modules/Online/types';
+import {OnlinePlayer, OnlineState} from '@/state/modules/Online/types';
+import {state as initialState} from './index';
 import {updateField} from 'vuex-map-fields';
+import {Card, GameStatus, Role} from '@/state/modules/Standalone/types';
+
+export const onlineMutations = {
+  peerOpen: `PEER_OPEN`,
+  resetState: `RESET_STATE`,
+  updateStatus: `UPDATE_STATUS`,
+  updateView: `UPDATE_VIEW`,
+  setLobbyId: `SET_LOBBY_ID`,
+  setHost: `SET_HOST`,
+  setUserPlayer: `SET_USER_PLAYER`,
+  updateUserPlayer: `UPDATE_USER_PLAYER`,
+  addPlayer: `ADD_PLAYER`,
+  updatePlayer: `UPDATE_PLAYER`,
+  setPlayers: `SET_PLAYERS`,
+  updateDrawDeck: `UPDATE_DRAW_DECK`,
+  updateDiscardDeck: `UPDATE_DISCARD_DECK`,
+  assignPlayerRoles: `ASSIGN_PLAYER_ROLES`,
+  setPresident: `SET_PRESIDENT`,
+  setChancellor: `SET_CHANCELLOR`,
+  resetFailedElectionsTracker: `RESET_FAILED_ELECTIONS_TRAKCER`,
+  increaseFailedElectionsTracker: `INCREASE_FAILED_ELECTIONS_TRACKER`,
+  drawCards: `DRAW_CARDS`,
+  discardCard: `DISCARD_CARD`,
+  discardDrawnCards: `DISCARD_DRAWN_CARDS`,
+  enactPolicy: `ENACT_POLICY`,
+  emptyDrawnCards: `EMPTY_DRAWN_CARDS`,
+  setLastGovernment: `SET_LAST_GOVERNMENT`,
+  executePlayer: `EXECUTE_PLAYER`,
+  setNextPresident: `SET_NEXT_PRESIDENT`,
+  setGameEnd: `SET_GAME_END`,
+};
 
 export const mutations: MutationTree<OnlineState> = {
   updateField,
+
+  [onlineMutations.peerOpen](state, { args }) {
+    const [id] = args;
+    state.user.peerId = id;
+  },
+
+  [onlineMutations.resetState](state) {
+    const {user, ...newState} = initialState();
+    Object.assign(state, newState);
+  },
+
+  [onlineMutations.updateStatus](state, status: GameStatus) {
+    state.status = status;
+  },
+
+  [onlineMutations.updateView](state, view: string) {
+    state.view = view;
+  },
+
+  [onlineMutations.setLobbyId](state, lobbyId: string) {
+    state.lobbyId = lobbyId;
+  },
+
+  [onlineMutations.setHost](state, isHost: boolean = true) {
+    state.user.isHost = isHost;
+  },
+
+  [onlineMutations.setUserPlayer](state, player: OnlinePlayer) {
+    state.user.player = player;
+  },
+
+  [onlineMutations.updateUserPlayer](state, payload: { name: string, isHost: boolean }) {
+    Object.assign(state.user.player, payload);
+  },
+
+  [onlineMutations.addPlayer](state, player: OnlinePlayer) {
+    state.players.push(player);
+  },
+
+  [onlineMutations.updatePlayer](state, player: OnlinePlayer) {
+    //
+  },
+
+  [onlineMutations.setPlayers](state, players: OnlinePlayer[]) {
+    state.players = players;
+  },
+
+  [onlineMutations.updateDrawDeck](state, deck: Card[]) {
+    state.game.drawPile = deck;
+  },
+
+  [onlineMutations.updateDiscardDeck](state, deck: Card[]) {
+    state.game.discardPile = deck;
+  },
+
+  [onlineMutations.assignPlayerRoles](state, roles: Role[]) {
+    state.players.map((player, index) => player.role = roles[index]);
+  },
 };
