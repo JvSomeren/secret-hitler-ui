3531c1bf3e2f2109d238c735b8090e9b0cdf8b58
Joost van Someren <joost.v.someren@gmail.com>
2019-08-20 09:30:49 +0200

    add types for online modules

--- src/state/modules/Online/types.ts
+++ src/state/modules/Online/types.ts
@@ -1,9 +1,51 @@
-import {Game, GameStatus, Player} from '@/state/modules/Standalone/types';
+import {Game, Player} from '@/state/modules/Standalone/types';
+
+export enum OnlineEvents {
+  UPDATE_VIEW = 'UPDATE_VIEW',
+  UPDATE_PLAYER_NAME = 'UPDATE_PLAYER_NAME',
+  UPDATE_PLAYERS = 'UPDATE_PLAYERS',
+}
+
+export enum GameStatus {
+  STOPPED = 'STOPPED',
+  LOBBY = 'LOBBY',
+  INITIALIZING = 'INITIALIZING', // start of game
+  SHOWING_ROLES = 'SHOWING_ROLES',
+  PRE_GAME = 'PRE_GAME',
+  CHOOSING_INITIAL_PRESIDENT = 'CHOOSING_INITIAL_PRESIDENT',
+  NOMINATING_CHANCELLOR = 'NOMINATING_CHANCELLOR', // start of round
+  VOTING_ON_GOVERNMENT = 'VOTING_ON_GOVERNMENT',
+  PRESIDENT_DISCARDING_POLICY = 'PRESIDENT_DISCARDING_POLICY',
+  CHANCELLOR_DISCARDING_POLICY = 'CHANCELLOR_DISCARDING_POLICY',
+  EXECUTIVE_ACTION = 'EXECUTIVE_ACTION', // end of round
+  ENACT_TOP_POLICY = 'ENACT_TOP_POLICY', // 3 failed elections in a row
+  INVESTIGATING_LOYALTY = 'INVESTIGATING_LOYALTY', // presidential powers
+  CALLING_SPECIAL_ELECTION = 'CALLING_SPECIAL_ELECTION',
+  POLICY_PEEKING = 'POLICY_PEEKING',
+  EXECUTING = 'EXECUTING',
+  CALLING_VETO = 'CALLING_VETO',
+  GAME_END = 'GAME_END', // end of game
+}
+
+export interface OnlinePlayer extends Player {
+  peer: string| null;
+  isHost: boolean;
+  send: (data: any) => void;
+}
+
+export interface User {
+  name: string;
+  peerId: string | null;
+  isHost: boolean;
+  player: OnlinePlayer | null;
+}
 
 export interface OnlineState {
   status: GameStatus;
   view: string;
   game: Game;
   playerCount: number;
-  players: Player[];
+  players: OnlinePlayer[];
+  user: User;
+  lobbyId: string;
 }
