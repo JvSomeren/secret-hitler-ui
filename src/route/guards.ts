0b4fbbc0ead0bcbec00276493d03cbb4563bb81f
Joost van Someren <joost.v.someren@gmail.com>
2019-08-20 09:29:52 +0200

    improve OnlineGuard

--- src/route/guards.ts
+++ src/route/guards.ts
@@ -1,6 +1,7 @@
 import {Route} from 'vue-router';
 import store from '@/state';
 import {GameStatus} from '@/state/modules/Standalone/types';
+import {onlineMutations} from '@/state/modules/Online/mutations';
 
 export function StandaloneGuard(to: Route, from: Route, next: any) {
   const gameState = store.getters['standalone/gameState'];
@@ -18,8 +19,18 @@ export function StandaloneGuard(to: Route, from: Route, next: any) {
 
 export function OnlineGuard(to: Route, from: Route, next: any) {
   const { isOnline } = store.state;
+  const gameState = store.getters['online/gameState'];
+  const { name } = store.getters['online/user'];
+
 
   if (!isOnline) return next({name: 'home'});
+  if (name === '') {
+    const query = to.params.hasOwnProperty('id') ? {lobbyId: to.params.id} : null;
+    return next({name: 'join', query});
+  } else if (to.name === 'online:lobby' && to.params.id !== gameState.lobbyId) {
+    store.commit(`online/${onlineMutations.resetState}`);
+    return next({name: 'join', query: {lobbyId: to.params.id}});
+  }
 
   next();
 }
